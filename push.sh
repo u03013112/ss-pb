58ed5c64142e06bfb9c9ea31a74d636cf8672c92
u03013112@hotmail.com <u03013112@hotmail.com>
2019-09-29 13:56:26 +0800

    添加push脚本，自动修改版本号

---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git pull &&
+if [ ! -f ./version ]; then
+    echo "1.0.0" > ./version
+    fi
+VERSION=$(cat ./version) && \
+echo "Current version: $VERSION" && \
+NEXT_VERSION=$(echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}') && \
+echo $NEXT_VERSION > ./version && \
+git add ./version && \
+    git commit -m "Released version v$NEXT_VERSION" && \
+    git tag -a v$NEXT_VERSION -m "Released version v$NEXT_VERSION" && \
+    git push origin v$NEXT_VERSION
+    git push
+esac
