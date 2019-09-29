beda1acc74bbbe241fd10052a94f7054717f00cb
u03013112@hotmail.com <u03013112@hotmail.com>
2019-09-29 17:42:42 +0800

    Released version v1.0.7

--- config/config.pb.gw.go
+++ config/config.pb.gw.go
@@ -41,6 +41,19 @@ func request_SSConfig_Test_0(ctx context.Context, marshaler runtime.Marshaler, c
 
 }
 
+func request_SSConfig_GetSSConfig_0(ctx context.Context, marshaler runtime.Marshaler, client SSConfigClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetSSConfigRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetSSConfig(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterSSConfigHandlerFromEndpoint is same as RegisterSSConfigHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterSSConfigHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -100,13 +113,46 @@ func RegisterSSConfigHandler(ctx context.Context, mux *runtime.ServeMux, conn *g
 
 	})
 
+	mux.Handle("POST", pattern_SSConfig_GetSSConfig_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_SSConfig_GetSSConfig_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SSConfig_GetSSConfig_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_SSConfig_Test_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "config", "test"}, ""))
+
+	pattern_SSConfig_GetSSConfig_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "config", "get-config"}, ""))
 )
 
 var (
 	forward_SSConfig_Test_0 = runtime.ForwardResponseMessage
+
+	forward_SSConfig_GetSSConfig_0 = runtime.ForwardResponseMessage
 )
--- push.sh
+++ push.sh
@@ -8,6 +8,7 @@ echo "Current version: $VERSION" && \
 NEXT_VERSION=$(echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}') && \
 echo $NEXT_VERSION > ./version && \
 git add ./version && \
+git add * && \
 git commit -m "Released version v$NEXT_VERSION" && \
 git tag -a v$NEXT_VERSION -m "Released version v$NEXT_VERSION" && \
 git push origin v$NEXT_VERSION
