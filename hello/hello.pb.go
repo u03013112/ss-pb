99e8957ae08518a3be44d3d0470dae62eb524e73
u03013112@hotmail.com <u03013112@hotmail.com>
2019-09-29 13:49:03 +0800

    修改脚本，重新制作hello

--- hello/hello.pb.go
+++ hello/hello.pb.go
@@ -17,6 +17,11 @@ import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 
+import (
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+)
+
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 var _ = fmt.Errorf
@@ -67,10 +72,84 @@ func init() {
 	proto.RegisterType((*HelloReply)(nil), "hello.HelloReply")
 }
 
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConn
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion4
+
+// Client API for Greeter service
+
+type GreeterClient interface {
+	// Sends a greeting
+	SayHello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error)
+}
+
+type greeterClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewGreeterClient(cc *grpc.ClientConn) GreeterClient {
+	return &greeterClient{cc}
+}
+
+func (c *greeterClient) SayHello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error) {
+	out := new(HelloReply)
+	err := grpc.Invoke(ctx, "/hello.Greeter/SayHello", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// Server API for Greeter service
+
+type GreeterServer interface {
+	// Sends a greeting
+	SayHello(context.Context, *HelloRequest) (*HelloReply, error)
+}
+
+func RegisterGreeterServer(s *grpc.Server, srv GreeterServer) {
+	s.RegisterService(&_Greeter_serviceDesc, srv)
+}
+
+func _Greeter_SayHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HelloRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GreeterServer).SayHello(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/hello.Greeter/SayHello",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GreeterServer).SayHello(ctx, req.(*HelloRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Greeter_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "hello.Greeter",
+	HandlerType: (*GreeterServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "SayHello",
+			Handler:    _Greeter_SayHello_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "hello/hello.proto",
+}
+
 func init() { proto.RegisterFile("hello/hello.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 138 bytes of a gzipped FileDescriptorProto
+	// 171 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xcc, 0x48, 0xcd, 0xc9,
 	0xc9, 0xd7, 0x07, 0x93, 0x7a, 0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0xac, 0x60, 0x8e, 0x92, 0x12,
 	0x17, 0x8f, 0x07, 0x88, 0x11, 0x94, 0x5a, 0x58, 0x9a, 0x5a, 0x5c, 0x22, 0x24, 0xc4, 0xc5, 0x92,
@@ -78,6 +157,8 @@ var fileDescriptor0 = []byte{
 	0x41, 0xd5, 0x14, 0xe4, 0x54, 0x0a, 0x49, 0x70, 0xb1, 0xe7, 0xa6, 0x16, 0x17, 0x27, 0xa6, 0xc3,
 	0x14, 0xc1, 0xb8, 0x46, 0xf6, 0x5c, 0xec, 0xee, 0x45, 0xa9, 0xa9, 0x25, 0xa9, 0x45, 0x42, 0x26,
 	0x5c, 0x1c, 0xc1, 0x89, 0x95, 0x60, 0x5d, 0x42, 0xc2, 0x7a, 0x10, 0x7b, 0x91, 0xed, 0x91, 0x12,
-	0x44, 0x15, 0x2c, 0xc8, 0xa9, 0x54, 0x62, 0x48, 0x62, 0x03, 0x3b, 0xcd, 0x18, 0x10, 0x00, 0x00,
-	0xff, 0xff, 0x00, 0xcc, 0xb4, 0x38, 0xaf, 0x00, 0x00, 0x00,
+	0x44, 0x15, 0x2c, 0xc8, 0xa9, 0x54, 0x62, 0x70, 0x52, 0x8a, 0x52, 0x48, 0xcf, 0x2c, 0xc9, 0x28,
+	0x4d, 0xd2, 0x4b, 0xce, 0xcf, 0xd5, 0x2f, 0x35, 0x30, 0x36, 0x30, 0x34, 0x36, 0x34, 0x34, 0xd2,
+	0x2f, 0x2e, 0xd6, 0x2d, 0x48, 0x82, 0xb8, 0x3e, 0x89, 0x0d, 0xec, 0x7c, 0x63, 0x40, 0x00, 0x00,
+	0x00, 0xff, 0xff, 0x9a, 0x52, 0x47, 0x15, 0xd3, 0x00, 0x00, 0x00,
 }
--- protoc.sh
+++ protoc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v $(pwd):$(pwd) -w $(pwd) znly/protoc --go_out=. -I. $1
+docker run --rm -v $(pwd):$(pwd) -v $(pwd):$(pwd)/github.com/u03013112/ss-pb -w $(pwd) znly/protoc --go_out=plugins=grpc:. -I. $1
