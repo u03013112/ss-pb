c5d678c336cac0e6080ede56eb47597f4928d1d7
u03013112 <u03013112@hotmail.com>
2020-01-03 19:15:29 +0800

    Released version v1.0.25

--- android/android.pb.gw.go
+++ android/android.pb.gw.go
@@ -54,6 +54,19 @@ func request_Android_GetConfig_0(ctx context.Context, marshaler runtime.Marshale
 
 }
 
+func request_Android_GetConfigNew_0(ctx context.Context, marshaler runtime.Marshaler, client AndroidClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetConfigRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetConfigNew(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_Android_Keepalive_0(ctx context.Context, marshaler runtime.Marshaler, client AndroidClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq KeepaliveRequest
 	var metadata runtime.ServerMetadata
@@ -194,6 +207,35 @@ func RegisterAndroidHandler(ctx context.Context, mux *runtime.ServeMux, conn *gr
 
 	})
 
+	mux.Handle("POST", pattern_Android_GetConfigNew_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Android_GetConfigNew_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Android_GetConfigNew_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_Android_Keepalive_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(ctx)
 		defer cancel()
@@ -318,6 +360,8 @@ var (
 
 	pattern_Android_GetConfig_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "android", "config"}, ""))
 
+	pattern_Android_GetConfigNew_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "android", "get-config"}, ""))
+
 	pattern_Android_Keepalive_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "android", "keepalive"}, ""))
 
 	pattern_Android_GetProdectionList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "android", "getProdectionList"}, ""))
@@ -331,6 +375,8 @@ var (
 	forward_Android_Login_0 = runtime.ForwardResponseMessage
 
 	forward_Android_GetConfig_0 = runtime.ForwardResponseMessage
+
+	forward_Android_GetConfigNew_0 = runtime.ForwardResponseMessage
 
 	forward_Android_Keepalive_0 = runtime.ForwardResponseMessage
 
