0ea75b017184586d196e5975b8407d712462c9f9
u03013112@hotmail.com <u03013112@hotmail.com>
2020-03-31 00:00:04 +0800

    Released version v1.0.35

--- config/config.pb.gw.go
+++ config/config.pb.gw.go
@@ -80,6 +80,19 @@ func request_SSConfig_GetSSLineConfig_0(ctx context.Context, marshaler runtime.M
 
 }
 
+func request_SSConfig_GetSSURL_0(ctx context.Context, marshaler runtime.Marshaler, client SSConfigClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetSSURLRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetSSURL(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterSSConfigHandlerFromEndpoint is same as RegisterSSConfigHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterSSConfigHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -226,6 +239,35 @@ func RegisterSSConfigHandler(ctx context.Context, mux *runtime.ServeMux, conn *g
 
 	})
 
+	mux.Handle("POST", pattern_SSConfig_GetSSURL_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_SSConfig_GetSSURL_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SSConfig_GetSSURL_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -237,6 +279,8 @@ var (
 	pattern_SSConfig_GetSSLineList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "config", "get-line-list"}, ""))
 
 	pattern_SSConfig_GetSSLineConfig_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "config", "get-line-config"}, ""))
+
+	pattern_SSConfig_GetSSURL_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "config", "getssurl"}, ""))
 )
 
 var (
@@ -247,4 +291,6 @@ var (
 	forward_SSConfig_GetSSLineList_0 = runtime.ForwardResponseMessage
 
 	forward_SSConfig_GetSSLineConfig_0 = runtime.ForwardResponseMessage
+
+	forward_SSConfig_GetSSURL_0 = runtime.ForwardResponseMessage
 )
